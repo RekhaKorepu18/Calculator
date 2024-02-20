836373427e7dcbe8dc2b0f2877816d289dfdeab9
Admin <admin@Admins-MacBook-Pro-10.local>
2024-02-20 13:09:30 +0530

    Added Subtraction feature from subbranch

---
+++ cal.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Subtraction{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value1:")
+        int a=sc.nextInt();
+        
+        System.out.println("Enter the value2:");
+        int b=sc.nextInt();
+        System.out.println("Result:",a-b);
+    }
+}
