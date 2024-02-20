b0ca9bac2a28178ed6a53a36ab83c8e056ce81b5
RekhaKorepu18 <160123097+RekhaKorepu18@users.noreply.github.com>
2024-02-20 14:22:40 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -1,7 +1,7 @@
 import java.util.*;
-class Subtraction{
 
-class Addition{
+
+class Operation{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the value1:")
